5d54750619148d32dd8535bd464281584175dd1d
Peter Steinfeld <psteinfeld@nvidia.com>
2020-09-25 09:03:17 -0700

    [flang] Failed call to CHECK() for call to ASSOCIATED(NULL())
    Calling "ASSOCATED(NULL()) was causing an internal check of the compiler to
    fail.
    
    I fixed this by changing the entry for "ASSOCIATED" in the intrinsics table to
    accept "AnyPointer" which contains a new "KindCode" of "pointerType".  I also
    changed the function "FromActual()" to return a typeless intrinsic when called
    on a pointer, which duplicates its behavior for BOZ literals.  This required
    changing the analysis of procedure arguments.  While testing processing for
    procedure arguments, I found another bad call to `CHECK()` which I fixed.
    
    I made several other changes:
      -- I implemented constant folding for ASSOCIATED().
      -- I fixed handling of NULL() in relational operations.
      -- I implemented semantic analysis for ASSOCIATED().
        -- I noticed that the semantics for ASSOCIATED() are similar to those for
           pointer assignment.  So I extracted the code that pointer assignment uses
           for procedure pointer compatibility to a place where it could be used by
           the semantic analysis for ASSOCIATED().
        -- I couldn't figure out how to make the general semantic analysis for
           procedure arguments work with ASSOCIATED()'s second argument, which can
           be either a pointer or a target.  So I stopped using normal semantic
           analysis for arguments for ASSOCIATED().
      -- I added tests for all of this.
    
    Differential Revision: https://reviews.llvm.org/D88313
    
    (cherry picked from commit c757418869c01f5ee08f05661debabbba92edcf9)

--- flang/lib/Semantics/expression.cpp
+++ flang/lib/Semantics/expression.cpp
@@ -2231,16 +2231,26 @@ std::optional<characteristics::Procedure> ExpressionAnalyzer::CheckCall(
           "References to the procedure '%s' require an explicit interface"_en_US,
           DEREF(proc.GetSymbol()).name());
     }
-    semantics::CheckArguments(*chars, arguments, GetFoldingContext(),
-        context_.FindScope(callSite), treatExternalAsImplicit);
-    const Symbol *procSymbol{proc.GetSymbol()};
-    if (procSymbol && !IsPureProcedure(*procSymbol)) {
-      if (const semantics::Scope *
-          pure{semantics::FindPureProcedureContaining(
-              context_.FindScope(callSite))}) {
-        Say(callSite,
-            "Procedure '%s' referenced in pure subprogram '%s' must be pure too"_err_en_US,
-            procSymbol->name(), DEREF(pure->symbol()).name());
+    // Checks for ASSOCIATED() are done in intrinsic table processing
+    bool procIsAssociated{false};
+    if (const SpecificIntrinsic *
+        specificIntrinsic{proc.GetSpecificIntrinsic()}) {
+      if (specificIntrinsic->name == "associated") {
+        procIsAssociated = true;
+      }
+    }
+    if (!procIsAssociated) {
+      semantics::CheckArguments(*chars, arguments, GetFoldingContext(),
+          context_.FindScope(callSite), treatExternalAsImplicit);
+      const Symbol *procSymbol{proc.GetSymbol()};
+      if (procSymbol && !IsPureProcedure(*procSymbol)) {
+        if (const semantics::Scope *
+            pure{semantics::FindPureProcedureContaining(
+                context_.FindScope(callSite))}) {
+          Say(callSite,
+              "Procedure '%s' referenced in pure subprogram '%s' must be pure too"_err_en_US,
+              procSymbol->name(), DEREF(pure->symbol()).name());
+        }
       }
     }
   }
@@ -2430,6 +2440,12 @@ MaybeExpr RelationHelper(ExpressionAnalyzer &context, RelationalOperator opr,
   if (analyzer.fatalErrors()) {
     return std::nullopt;
   } else {
+    if (IsNullPointer(analyzer.GetExpr(0)) ||
+        IsNullPointer(analyzer.GetExpr(1))) {
+      context.Say("NULL() not allowed as an operand of a relational "
+                  "operator"_err_en_US);
+      return std::nullopt;
+    }
     analyzer.ConvertBOZ(0, analyzer.GetType(1));
     analyzer.ConvertBOZ(1, analyzer.GetType(0));
     if (analyzer.IsIntrinsicRelational(opr)) {
