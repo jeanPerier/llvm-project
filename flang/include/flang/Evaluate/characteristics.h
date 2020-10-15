e9f80fcca11002fca87e5263421ae6564cc13987
Jean Perier <jperier@nvidia.com>
2020-10-15 06:08:45 -0700

    Add intents to intrinsic subroutine arguments

--- flang/include/flang/Evaluate/characteristics.h
+++ flang/include/flang/Evaluate/characteristics.h
@@ -233,6 +233,8 @@ struct DummyArgument {
       std::string &&, const Expr<SomeType> &, FoldingContext &);
   bool IsOptional() const;
   void SetOptional(bool = true);
+  common::Intent GetIntent() const;
+  void SetIntent(common::Intent);
   bool CanBePassedViaImplicitInterface() const;
   llvm::raw_ostream &Dump(llvm::raw_ostream &) const;
   // name and pass are not characteristics and so does not participate in
