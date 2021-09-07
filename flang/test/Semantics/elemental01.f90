b049f6f64ca48087c4c7d1f0e0c1414ec6d2c16d
peter klausler <pklausler@nvidia.com>
2021-09-07 12:44:41 -0700

    Update tests to new *.py infrastructure

--- flang/test/Semantics/elemental01.f90
+++ flang/test/Semantics/elemental01.f90
@@ -1,5 +1,4 @@
-! RUN: %S/test_errors.sh %s %t %flang_fc1
-! REQUIRES: shell
+! RUN: %python %S/test_errors.py %s %flang_fc1
 ! Tests ELEMENTAL subprogram constraints C15100-15102
 
 !ERROR: An ELEMENTAL subprogram must have at least one dummy argument
