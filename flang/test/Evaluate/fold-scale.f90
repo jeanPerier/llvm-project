37e53d56ea5f49206878d221313afd9dd8163928
Peter Klausler <pklausler@nvidia.com>
2021-12-29 16:22:37 -0800

    [flang] Fold SCALE()
    Fold references to the intrinsic function SCALE().

---
+++ flang/test/Evaluate/fold-scale.f90
@@ -0,0 +1,10 @@
+! RUN: %python %S/test_folding.py %s %flang_fc1
+! Tests folding of SCALE()
+module m
+  logical, parameter :: test_1 = scale(1.0, 1) == 2.0
+  logical, parameter :: test_2 = scale(0.0, 1) == 0.0
+  logical, parameter :: test_3 = sign(1.0, scale(-0.0, 1)) == -1.0
+  logical, parameter :: test_4 = sign(1.0, scale(0.0, 0)) == 1.0
+  logical, parameter :: test_5 = scale(1.0, -1) == 0.5
+end module
+
