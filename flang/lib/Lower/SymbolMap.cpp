6f3ae22b9b632ba79431985ee4692afb5cd05c67
Jean Perier <jperier@nvidia.com>
2022-10-14 02:32:25 -0700

    implement look-up in the symbolmap

--- flang/lib/Lower/SymbolMap.cpp
+++ flang/lib/Lower/SymbolMap.cpp
@@ -99,6 +99,22 @@ Fortran::lower::SymMap::lookupImpliedDo(Fortran::lower::SymMap::AcDoVar var) {
   return {};
 }
 
+llvm::Optional<fir::DefineFortranVariableOpInterface>
+Fortran::lower::SymMap::lookupVariableDefinition(semantics::SymbolRef sym) {
+  for (auto jmap = symbolMapStack.rbegin(), jend = symbolMapStack.rend();
+       jmap != jend; ++jmap) {
+    auto iter = jmap->find(&*sym);
+    if (iter != jmap->end()) {
+      if (const auto *varDef =
+              std::get_if<fir::DefineFortranVariableOpInterface>(&iter->second))
+        return *varDef;
+      else
+        return {};
+    }
+  }
+  return {};
+}
+
 llvm::raw_ostream &
 Fortran::lower::operator<<(llvm::raw_ostream &os,
                            const Fortran::lower::SymbolBox &symBox) {
