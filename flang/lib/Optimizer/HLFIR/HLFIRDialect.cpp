fbd59b2060f03b56b9794a629ac3cf536e89a1d6
Jean Perier <jperier@nvidia.com>
2022-10-19 13:01:36 -0700

    checkpoint

---
+++ flang/lib/Optimizer/HLFIR/HLFIRDialect.cpp
@@ -0,0 +1,18 @@
+//===-- HLFIRDialect.cpp --------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Coding style: https://mlir.llvm.org/getting_started/DeveloperGuide/
+//
+//===----------------------------------------------------------------------===//
+
+#include "flang/Optimizer/HLFIR/HLFIRDialect.h"
+
+#include "flang/Optimizer/HLFIR/HLFIRDialect.cpp.inc"
+
+#define GET_TYPEDEF_CLASSES
+#include "flang/Optimizer/Dialect/FIROpsTypes.cpp.inc"
