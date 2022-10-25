e2a9e776ee85f43ac39ef60ef21c17e1146edbb2
Jean Perier <jperier@nvidia.com>
2022-10-25 02:28:44 -0700

    Add HLFIROps.td

---
+++ flang/lib/Optimizer/HLFIR/HLFIROps.cpp
@@ -0,0 +1,22 @@
+//===-- HLFIROps.cpp ------------------------------------------------------===//
+//
+// Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+// See https://llvm.org/LICENSE.txt for license information.
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+//
+//===----------------------------------------------------------------------===//
+//
+// Coding style: https://mlir.llvm.org/getting_started/DeveloperGuide/
+//
+//===----------------------------------------------------------------------===//
+//
+#include "flang/Optimizer/HLFIR/HLFIROps.h"
+#include "mlir/IR/Builders.h"
+#include "mlir/IR/BuiltinTypes.h"
+#include "mlir/IR/DialectImplementation.h"
+#include "mlir/IR/Matchers.h"
+#include "mlir/IR/OpImplementation.h"
+#include "llvm/ADT/TypeSwitch.h"
+
+#define GET_OP_CLASSES
+#include "flang/Optimizer/HLFIR/HLFIROps.cpp.inc"
