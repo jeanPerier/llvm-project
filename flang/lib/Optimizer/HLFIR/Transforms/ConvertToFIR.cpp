9665b127347287e9f09014d6803f742fe0a68ab7
Jean Perier <jperier@nvidia.com>
2022-10-26 14:06:37 -0700

    Get array and scalar assignment working (but not that performant for arrays)

--- flang/lib/Optimizer/HLFIR/Transforms/ConvertToFIR.cpp
+++ flang/lib/Optimizer/HLFIR/Transforms/ConvertToFIR.cpp
@@ -8,16 +8,16 @@
 // This file defines a pass to lower HLFIR to FIR
 //===----------------------------------------------------------------------===//
 
-#include "flang/Optimizer/Builder/Todo.h"
-#include "flang/Optimizer/Builder/HLFIRTools.h"
 #include "flang/Optimizer/Builder/FIRBuilder.h"
+#include "flang/Optimizer/Builder/HLFIRTools.h"
+#include "flang/Optimizer/Builder/MutableBox.h"
+#include "flang/Optimizer/Builder/Runtime/Assign.h"
+#include "flang/Optimizer/Builder/Todo.h"
 #include "flang/Optimizer/Dialect/FIRDialect.h"
 #include "flang/Optimizer/Dialect/FIROps.h"
 #include "flang/Optimizer/Dialect/FIRType.h"
 #include "flang/Optimizer/HLFIR/HLFIROps.h"
 #include "flang/Optimizer/HLFIR/Passes.h"
-// TODO: needs to move pass into its own library because of this getKindMapping dependency...
-// Otherwise, there is a circular dependency.
 #include "flang/Optimizer/Support/FIRContext.h"
 #include "mlir/Transforms/DialectConversion.h"
 
@@ -30,6 +30,11 @@ using namespace mlir;
 
 namespace {
 
+static bool mayAlias(hlfir::FortranEntityLike lhs,
+                     hlfir::FortranEntityLike rhs) {
+  return true;
+}
+
 class AssignOpConversion : public mlir::OpRewritePattern<hlfir::AssignOp> {
 public:
   explicit AssignOpConversion(mlir::MLIRContext *ctx) : OpRewritePattern{ctx} {}
@@ -37,33 +42,66 @@ public:
   mlir::LogicalResult
   matchAndRewrite(hlfir::AssignOp assignOp,
                   mlir::PatternRewriter &rewriter) const override {
-    llvm::errs() << "rewritting assignOp\n";
+    // ! Note: this assumes allocatable assignment has been dealt with already.
     mlir::Location loc = assignOp->getLoc();
-    hlfir::FortranEntityLike lhs(assignOp.getLhs()); 
-    hlfir::FortranEntityLike rhs(assignOp.getRhs()); 
+    hlfir::FortranEntityLike lhs(assignOp.getLhs());
+    hlfir::FortranEntityLike rhs(assignOp.getRhs());
     auto module = assignOp->getParentOfType<mlir::ModuleOp>();
     fir::FirOpBuilder firOpBuilder(rewriter, fir::getKindMapping(module));
 
     if (rhs.getType().isa<hlfir::ExprType>())
-      TODO(loc, "hlfir.expr bufferization");
-    auto [lhsExv, lhsCleanUp] = hlfir::translateToExtendedValue(loc, firOpBuilder, lhs);
-    auto [rhsExv, rhsCleanUp] = hlfir::translateToExtendedValue(loc, firOpBuilder, rhs);
-    llvm::errs() << "reached 1\n";
+      TODO(loc, "hlfir.expr bufferization or inlining");
+    auto [rhsExv, rhsCleanUp] =
+        hlfir::translateToExtendedValue(loc, firOpBuilder, rhs);
+    auto [lhsExv, lhsCleanUp] =
+        hlfir::translateToExtendedValue(loc, firOpBuilder, lhs);
     if (lhsCleanUp || rhsCleanUp) {
-     // This should not be possible outside of the hlfir.expr LHS case. Add a TODO until the hlfir.expr case is dealt with.
-     TODO(loc, "cleanup in HLFIR assignment conversion");
+      // This should not be possible outside of the hlfir.expr LHS case. Add a
+      // TODO until the hlfir.expr case is dealt with.
+      TODO(loc, "cleanup in HLFIR assignment conversion");
     }
+
     if (lhs.isArray()) {
-      TODO(loc, "HLFIR array assignment conversion to FIR");
-      // TODO: for derived type assignments where the RHS is in memory and where we anyway call the runtime at the scalar level, Call the runtime at the array level directly instead.
+      // Just use the runtime
+      auto to = fir::getBase(firOpBuilder.createBox(loc, lhsExv));
+      auto from = fir::getBase(firOpBuilder.createBox(loc, rhsExv));
+      bool cleanUpTemp = false;
+      mlir::Type fromHeapType = fir::HeapType::get(
+          fir::unwrapRefType(from.getType().cast<fir::BoxType>().getEleTy()));
+      if (mayAlias(rhs, lhs)) {
+        /// Use the runtime to make a quick and dirty temp.
+        /// Overkill for scalar rhs that could be done in much more clever ways.
+        /// Note that temp descriptor must have the allocatable flag set so that
+        /// the runtime will attemp to reallocate.
+        mlir::Type fromBoxHeapType = fir::BoxType::get(fromHeapType);
+        auto fromMutableBox =
+            firOpBuilder.createTemporary(loc, fromBoxHeapType);
+        mlir::Value unallocatedBox = fir::factory::createUnallocatedBox(
+            firOpBuilder, loc, fromBoxHeapType, {});
+        firOpBuilder.create<fir::StoreOp>(loc, unallocatedBox, fromMutableBox);
+        fir::runtime::genAssign(firOpBuilder, loc, fromMutableBox, from);
+        cleanUpTemp = true;
+        from = firOpBuilder.create<fir::LoadOp>(loc, fromMutableBox);
+      }
+      auto toMutableBox = firOpBuilder.createTemporary(loc, to.getType());
+      // As per 10.2.1.2 point 1 (1) polymorphic variables must be allocatable.
+      // It is assumed here that they have been reallocated with the dynamic
+      // type and that the mutableBox will not be modified.
+      firOpBuilder.create<fir::StoreOp>(loc, to, toMutableBox);
+      fir::runtime::genAssign(firOpBuilder, loc, toMutableBox, from);
+      if (cleanUpTemp) {
+        mlir::Value addr =
+            firOpBuilder.create<fir::BoxAddrOp>(loc, fromHeapType, from);
+        firOpBuilder.create<fir::FreeMemOp>(loc, addr);
+      }
+    } else {
+      // Assume overlap does not matter for scalar (dealt with memmove for
+      // characters).
+      // FIXME: may be wrong if type is a derived type with
+      // "recursive" allocatable components (but this is what is currently
+      // done), in which case an overlap would matter.
+      fir::factory::genScalarAssignment(firOpBuilder, loc, lhsExv, rhsExv);
     }
-    llvm::errs() << "reached 2\n";
-    // Scalar assignment.
-    // Assume overlap does not matter for scalar (dealt with memmove for characters).
-    // FIXME: may be wrong if type is a derived type with
-    // "recursive" allocatable components (but this is what is currently done), in which case an overlap would matter.
-    fir::factory::genScalarAssignment(firOpBuilder, loc, lhsExv, rhsExv);
-    llvm::errs() << "reached 3\n";
     rewriter.eraseOp(assignOp);
     return mlir::success();
   }
@@ -71,7 +109,8 @@ public:
 
 class DeclareOpConversion : public mlir::OpRewritePattern<hlfir::DeclareOp> {
 public:
-  explicit DeclareOpConversion(mlir::MLIRContext *ctx) : OpRewritePattern{ctx} {}
+  explicit DeclareOpConversion(mlir::MLIRContext *ctx)
+      : OpRewritePattern{ctx} {}
 
   mlir::LogicalResult
   matchAndRewrite(hlfir::DeclareOp declareOp,
@@ -80,30 +119,47 @@ public:
     mlir::Value memref = declareOp.getMemref();
     fir::FortranVariableFlagsAttr fortranAttrs;
     if (auto attrs = declareOp.getFortranAttrs())
-      fortranAttrs = fir::FortranVariableFlagsAttr::get(rewriter.getContext(), *attrs);
-    auto originalBase = rewriter.create<fir::DeclareOp>(loc, memref.getType(), memref, declareOp.getShape(), declareOp.getTypeparams(), declareOp.getUniqName(), fortranAttrs).getResult();
+      fortranAttrs =
+          fir::FortranVariableFlagsAttr::get(rewriter.getContext(), *attrs);
+    auto originalBase = rewriter
+                            .create<fir::DeclareOp>(
+                                loc, memref.getType(), memref,
+                                declareOp.getShape(), declareOp.getTypeparams(),
+                                declareOp.getUniqName(), fortranAttrs)
+                            .getResult();
     mlir::Value hlfirBase;
     mlir::Type hlfirBaseType = declareOp.getBase().getType();
     if (hlfirBaseType.isa<fir::BaseBoxType>()) {
-      // TODO: optionality ? Need to define what is expected for the created box.
+      // TODO: optionality ? Need to define what is expected for the created
+      // box.
       //
       if (!originalBase.getType().isa<fir::BaseBoxType>()) {
         llvm::SmallVector<mlir::Value> typeParams;
-        auto maybeCharType = fir::unwrapSequenceType(fir::unwrapPassByRefType(hlfirBaseType)).dyn_cast<fir::CharacterType>();
+        auto maybeCharType =
+            fir::unwrapSequenceType(fir::unwrapPassByRefType(hlfirBaseType))
+                .dyn_cast<fir::CharacterType>();
         if (!maybeCharType || maybeCharType.hasDynamicLen())
-          typeParams.append(declareOp.getTypeparams().begin(), declareOp.getTypeparams().end());
-        hlfirBase = rewriter.create<fir::EmboxOp>(loc, hlfirBaseType, originalBase, declareOp.getShape(), /*slice=*/mlir::Value{}, typeParams);
+          typeParams.append(declareOp.getTypeparams().begin(),
+                            declareOp.getTypeparams().end());
+        hlfirBase = rewriter.create<fir::EmboxOp>(
+            loc, hlfirBaseType, originalBase, declareOp.getShape(),
+            /*slice=*/mlir::Value{}, typeParams);
       } else {
         // Rebox so that lower bounds are correct.
         // TODO: ensure "ones" lower bounds are respected here.
-        hlfirBase = rewriter.create<fir::ReboxOp>(loc, hlfirBaseType, originalBase, declareOp.getShape(), /*slice=*/mlir::Value{});
+        hlfirBase = rewriter.create<fir::ReboxOp>(
+            loc, hlfirBaseType, originalBase, declareOp.getShape(),
+            /*slice=*/mlir::Value{});
       }
     } else if (hlfirBaseType.isa<fir::BoxCharType>()) {
-      assert(declareOp.getTypeparams().size() == 1 && "must contain character length");
-      hlfirBase = rewriter.create<fir::EmboxCharOp>(loc, hlfirBaseType, originalBase, declareOp.getTypeparams()[0]);
+      assert(declareOp.getTypeparams().size() == 1 &&
+             "must contain character length");
+      hlfirBase = rewriter.create<fir::EmboxCharOp>(
+          loc, hlfirBaseType, originalBase, declareOp.getTypeparams()[0]);
     } else {
       if (hlfirBaseType != originalBase.getType()) {
-        declareOp.emitOpError() << "unhandled HLFIR variable type '"<< hlfirBaseType << "'\n";
+        declareOp.emitOpError()
+            << "unhandled HLFIR variable type '" << hlfirBaseType << "'\n";
         return mlir::failure();
       }
       hlfirBase = originalBase;
